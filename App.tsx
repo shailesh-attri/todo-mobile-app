c3b336e2b86abc170dcbd2c3c2b970dde1c7668f
Shailesh Attri <shailesh.attri@axieva.com>
2025-09-18 07:01:34 +0530

    setup initialized

--- App.tsx
+++ App.tsx
@@ -1,11 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 
 export default function App() {
+  const handleLogin = ()=>{
+
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={[styles.gradient, { backgroundColor: "#4c669f" }]} />
+      <View style={[styles.gradient, { backgroundColor: "#3b5998", opacity: 0.7 }]} />
+      <View style={[styles.gradient, { backgroundColor: "#192f6a", opacity: 0.5 }]} />
+
+      <Text style={styles.text}>Kuchh dekhna chahte ho?</Text>
+
+      <TouchableOpacity style={styles.login_button} onPress={handleLogin}>
+        <Text style={styles.login_text}>Login kar le</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -13,8 +22,29 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    position: "relative",
+  },
+  gradient: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  text: {
+    fontSize: 24,
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  login_button: {
+    backgroundColor: "#fff",
+    paddingVertical: 12,
+    paddingHorizontal: 30,
+    borderRadius: 10,
+  },
+  login_text: {
+    color: "#192f6a",
+    fontSize: 18,
+    fontWeight: "bold",
+    textTransform: "capitalize",
   },
 });
---
+++ pages/login/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+const Login = () => {
+  return (
+    <View>
+        Login
+    </View>
+  )
+}
+
+export default Login
