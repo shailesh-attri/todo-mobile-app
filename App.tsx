33121b4a4e6a5cabe1c3b9dc092c39aae22458cb
Shailesh attri <shaileshattri83@gmail.com>
2025-09-26 08:01:22 +0530

    Added : Signup screen and navigation

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { StackNavigationOptions } from '@react-navigation/stack'
 import Login from './screens/login/login'
+import Signup from './screens/signup/index'
 import Home from './screens'
 const Stack = createStackNavigator()
 
@@ -12,6 +13,7 @@ const App = () => {
       <Stack.Navigator>
         <Stack.Screen name="Home" component={Home} options={{ headerShown: false }}></Stack.Screen>
         <Stack.Screen name="Login" component={Login} ></Stack.Screen>
+        <Stack.Screen name="Signup" component={Signup} ></Stack.Screen>
       </Stack.Navigator>
     </NavigationContainer>
   )
